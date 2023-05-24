e5173b6a3edb0459f22f31d4add52a1bbfcd8fc9
Hao Nguyen <hao.nguyen@propine.com>
2023-05-24 09:08:16 +0700

    test

--- src/pages/api/daily-page/date.ts
+++ src/pages/api/daily-page/date.ts
@@ -1,4 +1,5 @@
 import { toSuccessResponse } from "@/common/helpers/toResponse";
+import { PinoLogger } from "@/common/logger/pino";
 import moment from "@/common/moment";
 import { NextApiRequest, NextApiResponse } from "next";
 type Data = {
@@ -9,6 +10,8 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   try {
+    const logger = new PinoLogger();
+    logger.info('get Date')
     res.status(200).json(toSuccessResponse(moment().format('YYYY/MM/DD hh:mm:ss')));
   } catch (error) {
     res.status(500).json({ message: (error as any).message || 'Internal server error' });
