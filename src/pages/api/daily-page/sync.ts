d38053a162ed2383b80e8feed0565f1c3eeef8ed
Hao Nguyen <hao.nguyen@propine.com>
2023-05-24 09:12:10 +0700

    test

--- src/pages/api/daily-page/sync.ts
+++ src/pages/api/daily-page/sync.ts
@@ -17,14 +17,17 @@ export default async function handler(
     const mqConnection = RabbitMQConnection.getConnection();
     const logger = new PinoLogger();
     logger.info('sync calling...')
-    new SyncSubcriber(
+    const subcriber = new SyncSubcriber(
       new DirectSubcriber(mqConnection),
       new ConfluenceAPI(),
       new DailyPageAPI(),
       logger
-    ).subcribe();
+    )
+    subcriber.subcribe()
     logger.info('subcribed sync...')
-    res.status(200).json(toSuccessResponse({ test: true }));
+    res.status(200).json(toSuccessResponse({
+      mqConnection, subcriber
+    }));
   } catch (error) {
     res.status(500).json({ message: (error as any).message || 'Internal server error' });
   }
